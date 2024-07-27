1f55369269dd6c9fabc8c41d3a7309bc13f3a5a7
kuthayoga295 <kuthayoga295@gmail.com>
2024-07-27 18:49:29 +0800

    new

--- .config/swayidle/swayidle.sh
+++ .config/swayidle/swayidle.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swayidle timeout 900 '$HOME/.config/swaylock/swaylock-effect.sh' timeout 910 'hyprctl dispatch dpms off' timeout 960 'systemctl suspend' resume 'hyprctl dispatch dpms on' before-sleep '$HOME/.config/swaylock/swaylock-effect.sh'
+swayidle timeout 900 '$HOME/.config/swaylock/swaylock-effect.sh' timeout 910 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 1200 'systemctl suspend' before-sleep '$HOME/.config/swaylock/swaylock-effect.sh'
--- .config/waybar/toggle-screen-recorder.sh
+++ .config/waybar/toggle-screen-recorder.sh
@@ -5,7 +5,7 @@ status=$?
 
 if [ $status != 0 ] 
 then 
-  wf-recorder -c libx264 -C libvorbis -x yuv420p --audio=easyeffects_sink.monitor -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4');
+  wf-recorder -c libx264 -x yuv420p --audio=easyeffects_sink.monitor -f $(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4');
 else 
   pkill --signal SIGINT wf-recorder
 fi;
